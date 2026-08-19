55df1fb85d9b01f91a9cc6be53ae7f27f2c70308
rahemih <rahemih0@gmail.com>
2026-08-19 16:15:21 -0700

    feat(step47-a3): wire integration configuration services

--- src/modules/integrations/integrations.module.ts
+++ src/modules/integrations/integrations.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ProviderConfigurationService } from './application/provider-configuration.service';
+import { EnvironmentSecretResolver } from './infrastructure/environment-secret.resolver';
+import { ProviderConfigurationRepository } from './infrastructure/provider-configuration.repository';
 
-@Module({})
+@Module({
+  providers:[
+    ProviderConfigurationRepository,
+    EnvironmentSecretResolver,
+    ProviderConfigurationService,
+  ],
+  exports:[ProviderConfigurationService],
+})
 export class IntegrationsModule {}
