26ff11a4f121410a1dd58068e54734d84860e96c
rahemih <rahemih0@gmail.com>
2026-08-19 06:44:26 -0700

    feat(marketing): export checkout promotion integration service

--- src/modules/marketing/marketing.module.ts
+++ src/modules/marketing/marketing.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { CampaignService } from './application/campaign.service';
 import { CouponEligibilityService } from './application/coupon-eligibility.service';
 import { AutomaticPromotionService } from './application/automatic-promotion.service';
+import { CheckoutPromotionService } from './application/checkout-promotion.service';
 import { CampaignRepository } from './infrastructure/campaign.repository';
 import { CouponEligibilityRepository } from './infrastructure/coupon-eligibility.repository';
 import { AutomaticPromotionRepository } from './infrastructure/automatic-promotion.repository';
 
 @Module({
-  providers: [CampaignService, CampaignRepository, CouponEligibilityService, CouponEligibilityRepository, AutomaticPromotionService, AutomaticPromotionRepository],
-  exports: [CampaignService, CouponEligibilityService, AutomaticPromotionService],
+  providers: [CampaignService, CampaignRepository, CouponEligibilityService, CouponEligibilityRepository, AutomaticPromotionService, AutomaticPromotionRepository, CheckoutPromotionService],
+  exports: [CampaignService, CouponEligibilityService, AutomaticPromotionService, CheckoutPromotionService],
 })
 export class MarketingModule {}
