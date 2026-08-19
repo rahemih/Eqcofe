c2ad98be8b5e137cb9973ae0a1330d9ccd98276b
rahemih <rahemih0@gmail.com>
2026-08-19 06:05:27 -0700

    feat(step46-a5): register coupon eligibility engine

--- src/modules/marketing/marketing.module.ts
+++ src/modules/marketing/marketing.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CampaignService } from './application/campaign.service';
+import { CouponEligibilityService } from './application/coupon-eligibility.service';
 import { CampaignRepository } from './infrastructure/campaign.repository';
+import { CouponEligibilityRepository } from './infrastructure/coupon-eligibility.repository';
 
 @Module({
-  providers: [CampaignService, CampaignRepository],
-  exports: [CampaignService],
+  providers: [CampaignService, CampaignRepository, CouponEligibilityService, CouponEligibilityRepository],
+  exports: [CampaignService, CouponEligibilityService],
 })
 export class MarketingModule {}
