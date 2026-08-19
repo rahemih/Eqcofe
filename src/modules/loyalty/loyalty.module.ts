1284973fa6b07874ae553f1956a5a8ca23b19665
rahemih <rahemih0@gmail.com>
2026-08-19 07:29:18 -0700

    feat(step46-a10): wire loyalty admin surface

--- src/modules/loyalty/loyalty.module.ts
+++ src/modules/loyalty/loyalty.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CustomerModule } from '../customer/customer.module';
 import { PointsService } from './application/points.service';
+import { LoyaltyAdminService } from './application/loyalty-admin.service';
 import { PointsRepository } from './infrastructure/points.repository';
+import { LoyaltyAdminController } from './presentation/loyalty-admin.controller';
 
 @Module({
   imports:[CustomerModule],
-  providers:[PointsRepository,PointsService],
-  exports:[PointsService],
+  controllers:[LoyaltyAdminController],
+  providers:[PointsRepository,PointsService,LoyaltyAdminService],
+  exports:[PointsService,LoyaltyAdminService],
 })
 export class LoyaltyModule {}
