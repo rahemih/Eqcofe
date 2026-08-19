5bf716a0cfeb93fc9f4c684279b2e14f3ff932c9
rahemih <rahemih0@gmail.com>
2026-08-19 06:00:01 -0700

    feat(step46-a4): register campaign lifecycle engine

--- src/modules/marketing/marketing.module.ts
+++ src/modules/marketing/marketing.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CampaignService } from './application/campaign.service';
+import { CampaignRepository } from './infrastructure/campaign.repository';
 
-@Module({})
+@Module({
+  providers: [CampaignService, CampaignRepository],
+  exports: [CampaignService],
+})
 export class MarketingModule {}
