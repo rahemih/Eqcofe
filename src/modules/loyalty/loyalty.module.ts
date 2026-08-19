13e72e476f6f9ecedfdb16b8bbd1aec2fde109f8
rahemih <rahemih0@gmail.com>
2026-08-19 07:10:04 -0700

    feat(step46-a9): wire loyalty points foundation

--- src/modules/loyalty/loyalty.module.ts
+++ src/modules/loyalty/loyalty.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CustomerModule } from '../customer/customer.module';
+import { PointsService } from './application/points.service';
+import { PointsRepository } from './infrastructure/points.repository';
 
-@Module({})
+@Module({
+  imports:[CustomerModule],
+  providers:[PointsRepository,PointsService],
+  exports:[PointsService],
+})
 export class LoyaltyModule {}
