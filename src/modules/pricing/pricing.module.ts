65389d98a69a5af104294eaf620530dfbac510ae
rahemih <rahemih0@gmail.com>
2026-08-21 02:55:33 -0700

    feat(step47-a7): wire FX preview orchestrator into pricing

--- src/modules/pricing/pricing.module.ts
+++ src/modules/pricing/pricing.module.ts
@@ -9,14 +9,16 @@ import { CurrencyPricingService } from './application/currency-pricing.service';
 import { ProfitGuardService } from './application/profit-guard.service';
 import { CurrencyImpactService } from './application/currency-impact.service';
 import { CurrencyRuleService } from './application/currency-rule.service';
+import { FxCurrencyPreviewService } from './application/fx-currency-preview.service';
 import { PRICING_PUBLIC_PORT } from './application/ports/pricing-public.port';
 import { PRICING_QUOTE_PORT } from './application/ports/pricing-quote.port';
 import { PRICING_COST_BASIS } from './application/ports/cost-basis.port';
 import { InventoryModule } from '../inventory/inventory.module';
 import { ConfigurationModule } from '../configuration/configuration.module';
+import { IntegrationsModule } from '../integrations/integrations.module';
 import { InventoryPricingCostBasisAdapter } from './infrastructure/inventory-cost-basis.adapter';
 @Module({
-  imports:[InventoryModule,ConfigurationModule],controllers:[PricingController],
-  providers:[PricingRepository,PricingQueryService,BasePriceService,PriceRuleService,BulkPricingService,CurrencyPricingService,CurrencyImpactService,CurrencyRuleService,ProfitGuardService,InventoryPricingCostBasisAdapter,{provide:PRICING_PUBLIC_PORT,useExisting:PricingQueryService},{provide:PRICING_QUOTE_PORT,useExisting:PricingQueryService},{provide:PRICING_COST_BASIS,useExisting:InventoryPricingCostBasisAdapter}],
+  imports:[InventoryModule,ConfigurationModule,IntegrationsModule],controllers:[PricingController],
+  providers:[PricingRepository,PricingQueryService,BasePriceService,PriceRuleService,BulkPricingService,CurrencyPricingService,CurrencyImpactService,CurrencyRuleService,FxCurrencyPreviewService,ProfitGuardService,InventoryPricingCostBasisAdapter,{provide:PRICING_PUBLIC_PORT,useExisting:PricingQueryService},{provide:PRICING_QUOTE_PORT,useExisting:PricingQueryService},{provide:PRICING_COST_BASIS,useExisting:InventoryPricingCostBasisAdapter}],
   exports:[PricingQueryService,PRICING_PUBLIC_PORT,PRICING_QUOTE_PORT],
 }) export class PricingModule{}
