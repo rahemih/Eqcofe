83f31b659ea961bbf6d12f8ade0f1dc6eab0f06a
rahemih <rahemih0@gmail.com>
2026-08-22 08:36:57 +0330

    feat(pos): wire Step 49 A9 admin API services

--- src/modules/pos/pos.module.ts
+++ src/modules/pos/pos.module.ts
@@ -7,15 +7,19 @@ import { PhysicalSaleService } from './application/physical-sale.service';
 import { PhysicalSaleCommitService } from './application/physical-sale-commit.service';
 import { OfflineCommandSyncService } from './application/offline-command-sync.service';
 import { OfflineReconciliationService } from './application/offline-reconciliation.service';
+import { PosAdminReconciliationService } from './application/pos-admin-reconciliation.service';
+import { PosOperationsService } from './application/pos-operations.service';
 import { PosScanResolutionService } from './application/pos-scan-resolution.service';
 import { PosInventoryConsumptionService } from './application/pos-inventory-consumption.service';
 import { PosPricingSnapshotService } from './application/pos-pricing-snapshot.service';
 import { PhysicalSaleRepository } from './infrastructure/physical-sale.repository';
 import { OfflineCommandRepository } from './infrastructure/offline-command.repository';
+import { PosAdminController } from './presentation/pos-admin.controller';
 
 @Module({
   imports: [CatalogModule, InventoryModule, PricingModule, PaymentsModule],
-  providers: [PhysicalSaleRepository, OfflineCommandRepository, PhysicalSaleService, PhysicalSaleCommitService, OfflineCommandSyncService, OfflineReconciliationService, PosScanResolutionService, PosInventoryConsumptionService, PosPricingSnapshotService],
-  exports: [PhysicalSaleService, PhysicalSaleCommitService, OfflineCommandSyncService, OfflineReconciliationService, PosScanResolutionService, PosInventoryConsumptionService, PosPricingSnapshotService],
+  controllers: [PosAdminController],
+  providers: [PhysicalSaleRepository, OfflineCommandRepository, PhysicalSaleService, PhysicalSaleCommitService, OfflineCommandSyncService, OfflineReconciliationService, PosAdminReconciliationService, PosOperationsService, PosScanResolutionService, PosInventoryConsumptionService, PosPricingSnapshotService],
+  exports: [PhysicalSaleService, PhysicalSaleCommitService, OfflineCommandSyncService, OfflineReconciliationService, PosAdminReconciliationService, PosOperationsService, PosScanResolutionService, PosInventoryConsumptionService, PosPricingSnapshotService],
 })
 export class PosModule {}
