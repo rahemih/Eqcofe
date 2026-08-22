e489016a2a4d345b3ffad69df79182e200b55eeb
rahemih <rahemih0@gmail.com>
2026-08-22 18:24:41 +0330

    feat(step50-a8): register Excel admin surface

--- src/modules/excel/excel.module.ts
+++ src/modules/excel/excel.module.ts
@@ -8,11 +8,14 @@ import { ImportRecoveryService } from './application/import-recovery.service';
 import { CatalogDryRunService } from './application/catalog-dry-run.service';
 import { CatalogApplyService } from './application/catalog-apply.service';
 import { PricingApplyService } from './application/pricing-apply.service';
+import { ExcelAdminService } from './application/excel-admin.service';
 import { ImportJobRepository } from './infrastructure/import-job.repository';
 import { ImportRecoveryRepository } from './infrastructure/import-recovery.repository';
+import { ExcelAdminController } from './presentation/excel-admin.controller';
 
 @Module({
   imports: [CatalogModule, PricingModule],
+  controllers: [ExcelAdminController],
   providers: [
     SafeWorkbookParserService,
     ExportTemplateService,
@@ -23,7 +26,8 @@ import { ImportRecoveryRepository } from './infrastructure/import-recovery.repos
     CatalogDryRunService,
     CatalogApplyService,
     PricingApplyService,
+    ExcelAdminService,
   ],
-  exports: [SafeWorkbookParserService, ExportTemplateService, ImportJobService, ImportRecoveryService, CatalogDryRunService, CatalogApplyService, PricingApplyService],
+  exports: [SafeWorkbookParserService, ExportTemplateService, ImportJobService, ImportRecoveryService, CatalogDryRunService, CatalogApplyService, PricingApplyService, ExcelAdminService],
 })
 export class ExcelModule {}
