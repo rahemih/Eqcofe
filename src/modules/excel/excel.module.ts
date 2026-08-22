e292cf231ac9573ea421b7ec88b3c365e56b2a2e
rahemih <rahemih0@gmail.com>
2026-08-22 17:27:11 +0330

    feat(step50-a6): wire pricing apply orchestration

--- src/modules/excel/excel.module.ts
+++ src/modules/excel/excel.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CatalogModule } from '../catalog/catalog.module';
+import { PricingModule } from '../pricing/pricing.module';
 import { SafeWorkbookParserService } from './application/safe-workbook-parser.service';
 import { ExportTemplateService } from './application/export-template.service';
 import { ImportJobService } from './application/import-job.service';
 import { CatalogDryRunService } from './application/catalog-dry-run.service';
 import { CatalogApplyService } from './application/catalog-apply.service';
+import { PricingApplyService } from './application/pricing-apply.service';
 import { ImportJobRepository } from './infrastructure/import-job.repository';
 
 @Module({
-  imports: [CatalogModule],
+  imports: [CatalogModule, PricingModule],
   providers: [
     SafeWorkbookParserService,
     ExportTemplateService,
@@ -16,7 +18,8 @@ import { ImportJobRepository } from './infrastructure/import-job.repository';
     ImportJobService,
     CatalogDryRunService,
     CatalogApplyService,
+    PricingApplyService,
   ],
-  exports: [SafeWorkbookParserService, ExportTemplateService, ImportJobService, CatalogDryRunService, CatalogApplyService],
+  exports: [SafeWorkbookParserService, ExportTemplateService, ImportJobService, CatalogDryRunService, CatalogApplyService, PricingApplyService],
 })
 export class ExcelModule {}
