a0e83428c3531ec733f9a8e7792475ac7eadaebf
rahemih <rahemih0@gmail.com>
2026-08-22 16:50:30 +0330

    feat(step50-a4): wire catalog dry-run service

--- src/modules/excel/excel.module.ts
+++ src/modules/excel/excel.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { CatalogModule } from '../catalog/catalog.module';
 import { SafeWorkbookParserService } from './application/safe-workbook-parser.service';
 import { ExportTemplateService } from './application/export-template.service';
 import { ImportJobService } from './application/import-job.service';
+import { CatalogDryRunService } from './application/catalog-dry-run.service';
 import { ImportJobRepository } from './infrastructure/import-job.repository';
 
 @Module({
-  providers: [SafeWorkbookParserService, ExportTemplateService, ImportJobRepository, ImportJobService],
-  exports: [SafeWorkbookParserService, ExportTemplateService, ImportJobService],
+  imports: [CatalogModule],
+  providers: [
+    SafeWorkbookParserService,
+    ExportTemplateService,
+    ImportJobRepository,
+    ImportJobService,
+    CatalogDryRunService,
+  ],
+  exports: [SafeWorkbookParserService, ExportTemplateService, ImportJobService, CatalogDryRunService],
 })
 export class ExcelModule {}
